76a21206a664641aa74c175d7bff6701da22c6bc
VortexProjects <vortex1649@gmail.com>
2023-12-28 16:51:24 +0530

    Modified the code by using PostgreSQL database

--- src/main/java/com/ananda/signupout/model/User.java
+++ src/main/java/com/ananda/signupout/model/User.java
@@ -6,16 +6,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
 @Component
+@Table(name = "auth_user") //For PostgreSQL (You need to chenge the table name from User to auth_user, because user is not allowed in PostgreSQL. If using MySQL no need to use this)
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Change from AUTO to IDENTITY
     private Integer id;
     
     @NotNull
